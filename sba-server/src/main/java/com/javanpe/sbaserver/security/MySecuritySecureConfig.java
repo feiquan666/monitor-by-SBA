5268979b03957a14ac9f53480451600946ccb5bc
飞拳 <17765678689@163.com>
2020-04-29 14:06:46 +0800

    增加了日志记录功能和自定义通知

--- sba-server/src/main/java/com/javanpe/sbaserver/security/MySecuritySecureConfig.java
+++ sba-server/src/main/java/com/javanpe/sbaserver/security/MySecuritySecureConfig.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.*;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 import java.util.UUID;
 
 /**
@@ -32,18 +33,29 @@ public class MySecuritySecureConfig extends WebSecurityConfigurerAdapter {
         successHandler.setDefaultTargetUrl(this.adminServer.path("/"));
 
         http.authorizeRequests()
+                // 允许访问静态资源和登录页面
                 .antMatchers(this.adminServer.path("/assets/**")).permitAll()
                 .antMatchers(this.adminServer.path("/login")).permitAll()
+                // 其他请求必须经过验证
                 .anyRequest().authenticated()
                 .and()
-                .formLogin().loginPage(this.adminServer.path("/login")).successHandler(successHandler).and()
-                .logout().logoutUrl(this.adminServer.path("/logout")).and()
-                .httpBasic().and()
+                // 配置登录和注销
+                .formLogin().loginPage(this.adminServer.path("/login")).successHandler(successHandler)
+                .and()
+                .logout().logoutUrl(this.adminServer.path("/logout"))
+                .and()
+                // 启用http-base支持，这是sba-client需要的
+                .httpBasic()
+                .and()
                 .csrf()
+                // 启用csrf保护
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 .ignoringRequestMatchers(
+                        // 禁用csrf保护的使用sba-client注册的端点
                         new AntPathRequestMatcher(this.adminServer.path("/instances"), HttpMethod.POST.toString()),
+                        // 禁用csrf保护的使用sba-client注册的端点
                         new AntPathRequestMatcher(this.adminServer.path("/instances/*"), HttpMethod.DELETE.toString()),
+                        // 禁用csrf保护Actuator的端点
                         new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
                 )
                 .and()
@@ -52,10 +64,18 @@ public class MySecuritySecureConfig extends WebSecurityConfigurerAdapter {
     }
 
     /**
-     * Required to provide UserDetailsService for "remember functionality"
+     * 通过Java代码配置用户,优先级高于配置文件
     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{bcrypt}$2a$10$GbkIrw7xOt50SXQu7tIGcemSWcD73MwMhLSrjCkyvFhssirdBOEsG").roles("USER");
+    }
+
+    /**
+     * 密码生成器
+    */
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("需要加密的密码"));
     }
 }
